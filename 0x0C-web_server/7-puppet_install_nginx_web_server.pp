d8ea6550dd5360804ce70a63e23cf7876cb391a1
saad ouchama <saadochama@gmail.com>
2023-09-28 03:27:13 -0700

    fix all tasks

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,6 +1,23 @@
-nstalls a Nginx server
+# Puppet manifest to install nginx
+package { 'nginx':
+  ensure => installed,
+}
 
-exec {'install':
-  provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/Tolulope05 permanent;/" /etc/nginx/sites-available/default ; sudo service nginx start',
+# Nginx config
+file_line { 'redirect_me':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
+
+# Nginx config
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
+
+# Nginx service
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
