58c7b483554d6a47f0afe33646143255613011ba
saad ouchama <saadochama@gmail.com>
2023-11-16 09:47:27 -0800

    init repositort

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,6 @@
+# fixes a file
+exec {'sets file limite for nginx':
+  command => 'sed -i "s/15/2000/g" /etc/default/nginx',
+  path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
+  onlyif  => 'test -f /etc/default/nginx'
+}
